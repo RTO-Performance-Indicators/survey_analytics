4995e03f607bb708611233638c98061cda09b807
Megan <Megan.Rooney87@gmail.com>
2021-02-09 13:21:33 +1100

    new, streamlined version

--- data requests/SSS_Query.py
+++ data requests/SSS_Query.py
@@ -4,69 +4,97 @@
 import pandas as pd
 import numpy as np
 import re
-#function to query measures or questions
-def Survey_query(survey,data,varnames, export_name=None, grouper=[], with_counts=0,supress_lowN=5,yearlast=False,rounded=False,unstack=False) :
-    #set weight and year vars
-    if survey == 'student':
-        year_var = 'SurveyYear'
-        weight_var = 'WEIGHT'
-    elif survey == 'employer':
-        year_var = 'Year'
-        weight_var = 'e_wght_delta'
-    #make list object to store results
-    result = []
-    counts = []
-    #add survey year to grouping variables
-    if yearlast == False:
-        grouper.insert(0,year_var)
-    else:
-        grouper.insert(len(grouper),year_var)
-    #loop through variables of interest and compute agree percentages
-    counter = 0
-    for i in varnames :
-        validobvs = data[(~np.isnan(data[i]))]
-        validobvs = data[data[i] >= 0]
-
-        Measurei_W = (validobvs[i]*validobvs[weight_var])  
-        validobvs[i + 'w'] = Measurei_W
-        if grouper is not None:
-            if ('TOID' in grouper) | ('RTOName' in grouper) | ('RTOTradingName' in grouper):
-                result.append(validobvs.groupby(grouper).apply(lambda x: x[i].sum()/x[i].count() if x[i].count() > supress_lowN else np.nan))
-            else:
-                result.append(validobvs.groupby(grouper).apply(lambda x: x[i +"w"].sum()/x[weight_var].sum() if (x[i].count() > supress_lowN) else np.nan))
-            
-            result[counter] = pd.DataFrame(result[counter], columns=[varnames[counter]])
-
-            if counter == 0:
-                result_df = result[counter]
-            else:
-                result_df = pd.DataFrame.join(result_df,result[counter])
-            if with_counts ==1:
-                counts.append(validobvs.groupby(grouper).apply(lambda x: x[i].count()))
-                count = pd.DataFrame(counts[counter], columns=[varnames[counter]+' N'])                
-                result_df = pd.DataFrame.join(result_df,count)
-        else:
-            result.append(validobvs.groupby([year_var]).apply(lambda x: x[i +"w"].sum()/x[weight_var].sum() if (x[i].count() > supress_lowN) else np.nan))
-            result[counter] = pd.DataFrame(result[counter], columns=[varnames[counter]])
-            
-            if counter == 0:
-                result_df = result[counter]
-            else:
-                result_df = pd.DataFrame.join(result_df,result[counter])
-            if with_counts ==1:
-                counts.append(validobvs.groupby([year_var]).apply(lambda x: x[i].count()))
-                count = pd.DataFrame(counts[counter], columns=[varnames[counter]+' N'])
-                result_df = pd.DataFrame.join(result_df,count)            
-        counter +=1
-    if rounded == True:
-        for i in varnames:
-            result_df[i] = [round(x*100,1) for x in result_df[i]]
-    if unstack == True:
-        result_df.unstack(year_var)        
-    if export_name is not None:
-        result_df.to_excel(savePath + export_name)
-    return result_df
-
-
 
 # %%
+
+# helper function for new survey_query. runs much faster then .apply
+# also wide version with for loop of columns is faster than doing on values column of melted df. no idea why though.
+
+def grouped_wm(data, variables, weight_var, grouper):
+
+    results = []
+    for value_col in variables:
+
+        data['product'] = data[value_col] * data[weight_var]
+        data['weighted_mean'] = data[weight_var].where(~data['product'].isnull())
+        grouped = data.groupby(grouper, sort=False).sum()
+        result = grouped['product'] / grouped['weighted_mean']
+        result.name = value_col
+        results.append(result)
+    data = pd.concat(results, axis=1) if len(results) > 1 else results[0]
+    return data
+# %%
+
+def Survey_query(data,survey='s',variables=[], grouper=[], count_type=None,supress_lowN=5,yearlast=False,rounded=False,combine_years=False,unweighted=False,force_weight=False, weight_var='WEIGHT', year_var='SurveyYear'):
+
+    # set survey-dependent variables. Weight and year default to combined student dataset versions, overrides if employer selected.
+    # might be computationally slower, but more user friendly to just specify 'e', not two different variable names.
+    # custom variable names for other datasets can be fed in as arguments and won't be changed unless survey argument is set to 'e'
+    
+    if survey == 's':
+        # do unweighted if data fitered to only one rto
+        # this is idiot proofing so even if the default weighted version is used, rto level data will only be weighted if you set force_weight to True
+        if (len(data['TOID'].unique()) == 1) & (force_weight==False):
+            unweighted = True   
+
+    elif survey == 'e':
+        # do unweighted if data fitered to only one rto
+        if len(data['e_toid'].unique()) == 1 & (force_weight==False):
+            unweighted = True
+        # set weight and year vars
+        year_var = 'year'
+        weight_var = 'Weight'
+
+    # do unweighted if grouped by rto (same reason as above)
+
+    if (any(item in grouper for item in ['TOID','RTOName','RTOTradingName','e_toid','rtoname','e_rto_name'])) & (force_weight==False):
+        unweighted = True
+
+    # add survey year to grouping variables in appropriate location
+    # again adding slight runtime to the function but makes it more user friendly as I want to group by year by default.
+    if combine_years == False:
+        if yearlast == False:
+            grouper.insert(0,year_var)
+        else:
+            grouper.append(year_var)
+    else:
+        # code assumes a grouper, adding a dummy column prevents errors with groupbys
+        # imo not worth adding ungrouped calculations instead as we rarely pool years of data (main exceptions being the CIF and COD toll which I wouldn't be using this for)
+        # (even though they'd only run if combine_years specified, it makes the function even bulkier to maintain).
+
+        data['group_dummy'] = 1
+        grouper.insert(0,'group_dummy')
+        
+    # filter data to relevant columns and valid values only
+
+    columns = data[variables]    
+    # replace invalid values with nan.
+    columns.mask((columns <0) | (columns >5),np.nan,inplace=True)
+    # make 5-point variables binary. Can't just use np.where because the NaNs would be evaluated and get changed to 0.
+    columns.replace(2,1,inplace=True)
+    columns.mask((columns > 2) ,0,inplace=True)
+    # create list of other columns we need (grouping and weight)
+    cols = grouper.copy()
+    cols.append(weight_var)
+    # merge cleaned columns with grouping variables and weights. Indexes match as they're from the same df to start with.
+    data = pd.merge(columns,data[cols],left_index=True,right_index=True)
+        
+    # unweighted version
+    if unweighted == True:
+        data.drop(columns=weight_var,inplace=True)
+        result = data.groupby(grouper,sort=False).mean()
+
+    #weighted version    
+    else: 
+       result = grouped_wm(data, variables, weight_var, grouper)
+
+    # suppress results with low N 
+    counts = data.groupby(grouper, sort=False).count()
+    low_n_indeces = counts[result.columns.values] < 5
+    result[low_n_indeces] = np.nan
+
+    # rounding
+    if rounded == True:
+        result = round(result*100,1)
+
+    return result
