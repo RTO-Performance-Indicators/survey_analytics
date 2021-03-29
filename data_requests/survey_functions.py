454404f1dfb0ce80450e6a99565d80446cb4ff98
Andrew Man Hay Chiu <amhchiu@gmail.com>
2021-03-29 11:09:00 +1100

    Update survey_functions.py

--- data_requests/survey_functions.py
+++ data_requests/survey_functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import dask
 
 def calc_prop(df, groups=[], vars=[], min_n=5, weighted=True):
 
@@ -10,19 +11,19 @@ def calc_prop(df, groups=[], vars=[], min_n=5, weighted=True):
     if weighted == True:
         weights = df['WEIGHT']
     else:
-        weights = [1] * len(df) # unweighted = all weights are 1
+        weights = [1] * len(df) # unweighted: all weights are equal to 1
     
     df['weight'] = weights
     group_vars.append('weight') # keeps weights as separate column
 
-    # Survey data is wide; convert to long
+    # Survey data is wide; convert to long format
     long = pd.melt(df, id_vars=group_vars, value_vars=calc_vars)
 
     # Remove invalid values (i.e. -888/-999 for unanswered/unpresented)
     long = long[(long['value'] >= 0) & (long['value'] <= 5)]
 
     # Convert from 5-scale likert to binary via merge
-    # NOTE: Has not effect binary columns, because 0 -> 0 and 1 -> 1
+    # NOTE: Does not affect binary variables, because 0 -> 0 and 1 -> 1
     likert_to_binary = pd.DataFrame({
         'value' : [0, 1, 2, 3, 4, 5], 
         'binary': [0, 1, 1, 0, 0, 0]
@@ -34,7 +35,7 @@ def calc_prop(df, groups=[], vars=[], min_n=5, weighted=True):
     group_vars.append('variable')
     
     # Calculate proportions and N
-    result_long = long.groupby(group_vars).apply(prop_n)
+    result_long = long.groupby(group_vars).apply(prop_n).compute()
 
     # Convert proportion to NA if N < min_n
     result_long.loc[result_long['N'] < min_n, 'proportion'] = np.nan
@@ -54,3 +55,29 @@ def prop_n(x):
 # groups = ['TOID']
 # measures = ['Measure']
 # calc_prop(df=data, groups=groups, vars=measures, weighted=False)
+
+
+# Create a larger test dataset
+import random
+size = 1000000
+data = pd.DataFrame({
+    'TOID': [random.randint(1, 3) for i in range(size)],
+    'Measure': [random.randint(0, 1) for i in range(size)],
+    'WEIGHT': [random.random() for i in range(size)]
+})
+
+import time
+calc_prop(df=data, groups=groups, vars=measures, weighted=False)
+timeit calc_prop(df=data, groups=groups, vars=measures, weighted=False)
+
+data = dask.dataframe({
+    'TOID': [random.randint(1, 3) for i in range(size)],
+    'Measure': [random.randint(0, 1) for i in range(size)],
+    'WEIGHT': [random.random() for i in range(size)]
+})
+
+import dask.dataframe as dd
+
+data = dd.from_pandas(data, npartitions=2)
+
+data
