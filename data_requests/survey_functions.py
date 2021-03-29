2bd6cfe362033c1f7b63445123f26106bcbc82a3
Andrew Man Hay Chiu <amhchiu@gmail.com>
2021-03-30 10:44:17 +1100

    Update survey_functions.py
    Added dask version of calc_prop function.
    
    It isn't faster than normal pandas version.

--- data_requests/survey_functions.py
+++ data_requests/survey_functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import dask
+import dask.dataframe as dd
 
 def calc_prop(df, groups=[], vars=[], min_n=5, weighted=True):
 
@@ -58,26 +59,67 @@ def prop_n(x):
 
 
 # Create a larger test dataset
-import random
-size = 1000000
-data = pd.DataFrame({
-    'TOID': [random.randint(1, 3) for i in range(size)],
-    'Measure': [random.randint(0, 1) for i in range(size)],
-    'WEIGHT': [random.random() for i in range(size)]
-})
+# import random
+# size = 1000000
+# data = pd.DataFrame({
+#     'TOID': [random.randint(1, 3) for i in range(size)],
+#     'Measure': [random.randint(0, 1) for i in range(size)],
+#     'WEIGHT': [random.random() for i in range(size)]
+# })
 
-data.set_index('TOID')
+# import time
+# groups = ['TOID']
+# measures = ['Measure']
+# calc_prop(df=data, groups=groups, vars=measures, weighted=False)
+# %timeit calc_prop(df=data, groups=groups, vars=measures, weighted=False)
 
-import time
-groups = ['TOID']
-measures = ['Measure']
-calc_prop(df=data, groups=groups, vars=measures, weighted=False)
-timeit calc_prop(df=data, groups=groups, vars=measures, weighted=False)
+# DASK version
+def calc_prop_dask(df, groups=[], vars=[], min_n=5, weighted=True):
+    
+    # Take copies of lists to avoid modifying global/environment variables
+    group_vars = groups.copy()
+    calc_vars = vars.copy()
 
+    if weighted == True:
+        weights = df['WEIGHT']
+    else:
+        weights = [1] * len(df) # unweighted: all weights are equal to 1
+    
+    df['weight'] = weights
+    group_vars.append('weight') # keeps weights as separate column
 
-import dask.dataframe as dd
+    # Convert to dask dataframe
+    df = dd.from_pandas(df, npartitions=4)
 
-data2 = dd.from_pandas(data, npartitions=4)
+    # Survey data is wide; convert to long format
+    # long = pd.melt(df, id_vars=group_vars, value_vars=calc_vars)
+    long = df.melt(id_vars=group_vars, value_vars=calc_vars)
 
-data2
+    # Remove invalid values (i.e. -888/-999 for unanswered/unpresented)
+    long = long[(long['value'] >= 0) & (long['value'] <= 5)]
 
+    # Convert from 5-scale likert to binary via merge
+    # NOTE: Does not affect binary variables, because 0 -> 0 and 1 -> 1
+    likert_to_binary = pd.DataFrame({
+        'value' : [0, 1, 2, 3, 4, 5], 
+        'binary': [0, 1, 1, 0, 0, 0]
+    })
+    likert_to_binary = dd.from_pandas(likert_to_binary, npartitions=1)
+    long = long.merge(likert_to_binary)
+
+    # Conversion to long format gathers measures into one column, 'variable'
+    # 'variable' column needs to be added as a grouping variable
+    group_vars.append('variable')
+
+    # Calculate proportions and N
+    result_long = long.groupby(group_vars).apply(prop_n, meta={'proportion': 'float', 'N': 'int'}).compute()
+
+    # Convert proportion to NA if N < min_n
+    result_long.loc[result_long['N'] < min_n, 'proportion'] = np.nan
+
+    return(result_long)
+
+# `calc_prop_dask(df=data, groups=groups, vars=measures, weighted=False)
+
+# %timeit calc_prop(df=data, groups=groups, vars=measures, weighted=False)
+# %timeit calc_prop_dask(df=data, groups=groups, vars=measures, weighted=False)`
