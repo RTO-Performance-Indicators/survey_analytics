572667873830a5a84d5d58e293e537ec179723b5
AndrewManHayChiu <amhchiu@gmail.com>
2021-04-27 10:47:42 +1000

    Update spacy_test.py

--- text_analytics/spacy_test.py
+++ text_analytics/spacy_test.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-
-import hunspell
+import re
 
 import gensim
 import gensim.corpora as corpora
@@ -9,25 +8,26 @@ from gensim.utils import simple_preprocess
 from gensim.models import CoherenceModel
 
 import spacy
-from spacy.language import Language
-from spacy.lang.en import English
-from spacy.lang.en.stop_words import STOP_WORDS
-
-spacy_stopwords = spacy.lang.en.stop_words.STOP_WORDS
 
 # Load small trained pipeline that is used to predict POS tags and dependencies
 # Can use en_core_web_lg for a larger trained pipeline
 # nlp = spacy.load('en_core_web_sm')
-nlp = spacy.load('en_core_web_lg')
+# nlp = spacy.load('en_core_web_md')
+nlp = spacy.load('en_core_web_lg') # English, web-based library, large
 
 # Test data
 data = pd.DataFrame({
-    'SurveyResponseID': [1, 2, 3, 4],
     's_rsn_dc_v': [
         'Testing the spacy package', 
         'I was really unsatisfied with the poor quality of teachers', 
         'The teachers and trainors at Chisholm Institute were bulying the students',
-        'This course was a waste of time! Teachers do not turn up on time, and students get bullied all the time']
+        'This course was a waste of time! Teachers do not turn up on time, and students get bullied all the time',
+        'Teachers are not professional.',
+        'Admin did not help me re-enrol into the course',
+        'Teachers were fantastic! The course helped me overcome my anxiety',
+        'qualiry of teachhers is very poor',
+        'THe assessments were inconsistent'
+    ]
 })
 
 # Run pipeline:
@@ -36,19 +36,21 @@ data['doc'] = [nlp(text) for text in data['s_rsn_dc_v']]
 
 # Number of tokens in each doc
 data['n_tokens'] = [len(tokens) for tokens in data['doc']]
-data
-
 
 # Sentences per doc
-list(data['doc'][3].sents)
-for doc in data['doc']:
-    print(len(list(doc.sents)))
 data['n_sentences'] = [len(list(doc.sents)) for doc in data['doc']]
+
 data
 
 # Part of Speech (POS) tags (already identified in nlp function above)
 for token in data['doc'][1]:
     print(token.text, token.pos_)
+
+data['doc'][1][1].pos_
+
+# Morphology
+for token in data['doc'][1]:
+    print(token.text, token.morph)
 
 # Lemma
 for token in data['doc'][1]:
@@ -62,41 +64,66 @@ for token in data['doc'][1]:
 for ent in data['doc'][2].ents:
     print(ent.text, ent.label_)
 
+# # Add a pipe to the pipeline to remove stop words
+# @Language.component('stopwords')
+# def component_func(doc):
+#     doc = [token.text for token in doc if token.is_stop != True]
+#     return(doc)
 
-# https://towardsdatascience.com/building-a-topic-modeling-pipeline-with-spacy-and-gensim-c5dc03ffc619
+# nlp.add_pipe('stopwords', last=True)
 
-# Add a pipe to the pipeline to remove stop words
-@Language.component('stopwords')
-def component_func(doc):
-    doc = [token.text for token in doc if token.is_stop != True]
-    return(doc)
+# # Rerun the nlp on the data with the modified pipeline
+# data['doc'] = [nlp(text) for text in data['s_rsn_dc_v']]
+# data
 
-nlp.add_pipe('stopwords')
+# Spelling errors
+doc = nlp(u"trainers and trainors")
+trainer = doc[0]
+trainor = doc[2]
+trainer.similarity(trainor)
+
+
+# Get only tokens that satisfy custom criteria
+# 1 - Lemma
+# 2 - Nouns (identified in POS tagging)
+[token.lemma_ for token in data['doc'][1] if token.pos_ == 'NOUN']
+
+noun_list = []
+for doc in data['doc']:
+    nouns = [token.lemma_ for token in doc if token.pos_ == 'NOUN']
+    noun_list.append(nouns)
+
+data['nouns'] = noun_list
+data
+
+# Topic Modelling with gensim
+# https://www.machinelearningplus.com/nlp/topic-modeling-gensim-python/
 
 # For Gensim, spacy docs need to be converted into a list of lists
-doc_list = []
-for doc in data['s_rsn_dc_v']:
-    doc_strings = nlp(doc)
-    doc_list.append(doc_strings)
+# doc_list = []
+# for doc in data['s_rsn_dc_v']:
+#     doc_strings = nlp(doc)
+#     doc_list.append(doc_strings)
 
-doc_list
+# doc_list
 
-words = corpora.Dictionary(doc_list)
-
-# Turn each document into bag of words
-corpus = [words.doc2bow(doc) for doc in doc_list]
+# Create dictionary and corpus
+dictionary = corpora.Dictionary(data['nouns'])
+corpus = [dictionary.doc2bow(noun) for noun in data['nouns']]
 corpus
 
 # Run LDA model
 lda_model = gensim.models.ldamodel.LdaModel(
     corpus=corpus, 
-    id2word=words, 
-    num_topics=2,
-    random_state=2,
-    update_every=1,
-    passes=2,
-    alpha='auto',
-    per_word_topics=True
+    id2word=dictionary, 
+    num_topics=3,
 )
 
 lda_model
+
+
+# Topic Evaluation
+# 1 - Observer Coherence measure
+# 2 - Word Intrusion  detection
+
+# Topic reliability/consistency
