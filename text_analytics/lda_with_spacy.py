7ce306147df07570b1149241f6733ffe52fcc5a2
AndrewManHayChiu <amhchiu@gmail.com>
2021-05-10 08:30:04 +1000

    Add joblib functionality to parallelize spacy

--- text_analytics/lda_with_spacy.py
+++ text_analytics/lda_with_spacy.py
@@ -10,6 +10,31 @@ from gensim.models import CoherenceModel
 import spacy
 import contextualSpellCheck
 
+from joblib import Parallel, delayed
+
+# https://prrao87.github.io/blog/spacy/nlp/performance/2020/05/02/spacy-multiprocess.html#Option-3:-Parallelize-the-work-using-joblib
+def chunker(iterable, total_length, chunksize):
+    return (iterable[pos: pos + chunksize] for pos in range(0, total_length, chunksize))
+
+def flatten(list_of_lists):
+    # Flatten a list of lists to a combined list
+    return [item for sublist in list_of_lists for item in sublist]
+
+def process_chunk(texts):
+    preproc_pipe = []
+    # for doc in nlp.pipe(texts, batch_size=20):
+    #     preproc_pipe.append(lemmatize_pipe(doc))
+    for text in texts:
+        preproc_pipe.append(nlp.pipe(texts, batch_size=20))
+    return preproc_pipe
+
+def preprocess_parallel(texts, chunksize=100):
+    executor = Parallel(n_jobs=7, backend='multiprocessing', prefer="processes")
+    do = delayed(process_chunk)
+    tasks = (do(chunk) for chunk in chunker(texts, len(texts), chunksize=chunksize))
+    result = executor(tasks)
+    return flatten(result)
+
 # Load data
 data = pd.read_csv('S:\\RTOPI\\Both Surveys\\All Final Datasets\\Datasets - 2020\\Output\\StudentSurveys.csv', encoding='ISO-8859-1')
 
@@ -19,6 +44,8 @@ verbatims = verbatims.replace(np.NaN, '')
 
 verbatims['verbatims_combined'] = verbatims['s_rsn_dc_v'] + verbatims['s_rsn_rc_v'] + verbatims['s_imp_v']
 verbatims
+
+test = verbatims[0:100]
 
 # Run pipeline
 # nlp = spacy.load('en_core_web_sm')
@@ -44,3 +71,7 @@ def pos(df, required_tags):
 
 required_tags = ['NOUN', 'ADJ', 'VERB']
 test = pos(verbatims, required_tags=required_tags)
+
+%%time
+test['preproc_parallel'] = preprocess_parallel(test['verbatims_combined'], chunksize=10)
+
